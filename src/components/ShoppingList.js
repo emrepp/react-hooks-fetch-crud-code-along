5d141c6aca065368bb8d2eca851e3dceb059588b
Emma Repp <emmarepp.draws@gmail.com>
2022-10-03 14:08:43 -1000

    done

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,19 @@
-import React, { useState } from "react";
+import React, { useEffect,useState } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
 
+
 function ShoppingList() {
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/items")
+    .then((request)=> request.json())
+    .then((items) => setItems(items));
+  }, []
+  );
 
   function handleCategoryChange(category) {
     setSelectedCategory(category);
